9ffe595605a9bd83e5775d6afd2788d31f5bfd95
joaoPeedro <joao.p.baltazar@gmail.com>
2023-12-19 17:10:24 +0000

    Update app.js formated

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3120;
 
 // Lista de IPs autorizados na whitelist
-const whitelist = ['192.168.1.1', '10.0.0.1', 'http://localhost:3120', 'http://localhost:3000', 'https://app-node-beta.vercel.app/'];
+const whitelist = [
+  "192.168.1.1",
+  "10.0.0.1",
+  "http://localhost:3120",
+  "http://localhost:3000",
+  "https://app-node-beta.vercel.app/",
+];
 
 // Endereço IP da sua VPN
-const vpnIpAddress = '93.176.86.249'; // Substitua pelo IP da sua VPN
+const vpnIpAddress = ["93.176.86.249", "127.0.0.1"]; // Substitua pelo IP da sua VPN
 
 // Middleware para verificar o IP
 const checkVpnIp = (req, res, next) => {
   const clientIp = req.ip; // Obtém o IP do cliente da solicitação
 
-  console.log('CLIENT IP',{clientIp});
+  console.log("CLIENT IP", { clientIp });
 
   // Verifica se o IP está na whitelist (sua VPN)
-  if (clientIp === vpnIpAddress) {
+  if (vpnIpAddress.includes(clientIp)) {
     next(); // IP autorizado, permite acesso
   } else {
-    res.status(403).send('Acesso proibido'); // IP não autorizado, bloqueia acesso
+    res.status(403).send("Acesso proibido"); // IP não autorizado, bloqueia acesso
   }
 };
 
@@ -30,13 +36,13 @@ app.use(checkVpnIp);
 const whitelistMiddleware = (req, res, next) => {
   const clientIp = req.ip; // Obtém o IP do cliente da solicitação
 
-  console.log(clientIp)
+  console.log(clientIp);
 
   // Verifica se o IP está na whitelist
   if (whitelist.includes(clientIp)) {
     next(); // IP autorizado, permite acesso
   } else {
-    res.status(403).send('Acesso proibido IP em falta'); // IP não autorizado, bloqueia acesso
+    res.status(403).send("Acesso proibido IP em falta"); // IP não autorizado, bloqueia acesso
   }
 };
 
@@ -47,24 +53,23 @@ const whitelistMiddleware = (req, res, next) => {
 // app.use(cors());
 
 // Rota protegida pela whitelist
-app.get('/', (req, res) => {
-  res.send('Hello, World! IP autorizado.');
+app.get("/", (req, res) => {
+  res.send("Hello, World! IP autorizado.");
 });
 
 const corsOptions = {
   origin: function (origin, callback) {
-
-    console.log({origin});
+    console.log({ origin });
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
 };
 
-app.get('/batatas', cors(corsOptions), (req, res) => {
-  res.send({message:'Hello, World batatas!'});
+app.get("/batatas", cors(corsOptions), (req, res) => {
+  res.send({ message: "Hello, World batatas!" });
 });
 
 app.listen(port, () => {
